2259c1506488459da54f7d85e4582bec8f27a2fc
manchikooo <manchiko.man31@mail.ru>
2022-01-21 14:11:48 +0300

    made button for change clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
+import {AnalogClock} from "./Clock/AnalogClock";
+import {DigitalClock} from "./Clock/DigitalClock";
+import styles from "./Clock/Clock.module.scss"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [toggleClock, setToggleClock] = useState<boolean>(true)
+
+    return (
+        <div className="App">
+            {toggleClock ? <AnalogClock/> : <DigitalClock/>}
+            <div className={styles.buttonStyle} onClick={() => setToggleClock(!toggleClock)}>
+                <div>change clock</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Clock/AnalogClock.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react';
+import styles from './Clock.module.scss'
+
+type ClockPropsType = {}
+const getTime = (num: number) => num < 10 ? '0' + num : num
+
+export const AnalogClock = (props: ClockPropsType) => {
+
+    const [date, setDate] = useState<Date>(new Date())
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+        return () => clearInterval(intervalID)
+    }, [])
+
+    const deg = 6
+
+    const hoursString = getTime(date.getHours() * 30)
+    const minutesString = getTime(date.getMinutes() * deg)
+    const secondsString = getTime(date.getSeconds() * deg)
+
+    return (
+        <div className={styles.clockWrapper}>
+            <div className={styles.clockContainer}>
+                <div className={styles.clock}>
+                    <div className={styles.hour}>
+                        <div className={styles.hours}
+                             style={{transform: `rotateZ(${+hoursString + (+minutesString / 12)}deg)`}}>{}</div>
+                    </div>
+                    <div className={styles.minute}>
+                        <div className={styles.minutes} style={{transform: `rotateZ(${+minutesString}deg)`}}>{}</div>
+                    </div>
+                    <div className={styles.second}>
+                        <div className={styles.seconds} style={{transform: `rotateZ(${+secondsString}deg)`}}>{}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
