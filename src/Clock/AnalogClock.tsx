3bcdafa25028a9a6db5e657de2a4fbc29efff00e
manchikooo <manchiko.man31@mail.ru>
2022-01-21 22:44:04 +0300

    при уменьшении размера стрелки не улетают. нормальное поведение

--- src/Clock/AnalogClock.tsx
+++ src/Clock/AnalogClock.tsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import styles from './Clock.module.scss'
 
-type ClockPropsType = {}
+type ClockPropsType = {
+
+}
 const getTime = (num: number) => num < 10 ? '0' + num : num
 
 export const AnalogClock = (props: ClockPropsType) => {
@@ -24,20 +26,16 @@ export const AnalogClock = (props: ClockPropsType) => {
 
     return (
         <div className={styles.clockWrapper}>
-            <div className={styles.clockContainer}>
-                <div className={styles.clock}>
-                    <div className={styles.hour}>
-                        <div className={styles.hours}
-                             style={{transform: `rotateZ(${+hoursString + (+minutesString / 12)}deg)`}}>{}</div>
-                    </div>
-                    <div className={styles.minute}>
-                        <div className={styles.minutes} style={{transform: `rotateZ(${+minutesString}deg)`}}>{}</div>
-                    </div>
-                    <div className={styles.second}>
-                        <div className={styles.seconds} style={{transform: `rotateZ(${+secondsString}deg)`}}>{}</div>
-                    </div>
+                <div className={styles.hour}>
+                    <div className={styles.hours}
+                         style={{transform: `rotateZ(${+hoursString + (+minutesString / 12)}deg)`}}>{}</div>
                 </div>
-            </div>
+                <div className={styles.minute}>
+                    <div className={styles.minutes} style={{transform: `rotateZ(${+minutesString}deg)`}}>{}</div>
+                </div>
+                <div className={styles.second}>
+                    <div className={styles.seconds} style={{transform: `rotateZ(${+secondsString}deg)`}}>{}</div>
+                </div>
         </div>
     )
 };
